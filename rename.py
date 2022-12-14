fd1f80f5e918e79604a9116576c3c602702855e4
Saim Khalid <saimkhalid650@gmail.com>
2022-12-14 15:40:12 +0500

    Update rename.py

--- rename.py
+++ rename.py
@@ -2,15 +2,14 @@ import os
 import glob
 
 def main():
-    path = "/Users/saimkhalid/Documents/iOPTIME Resources/Tuber Dataset/Data_Augmentation/Augmented_dataset/Random Brightness Contrast/"
-    suffix = '.png'
+    path = "Add Dataset Path/" # path to your dataset
+    suffix = '.png' #change to any Image format
     files = glob.glob(path + '*' + suffix)
     for idx, filename in enumerate(sorted(files)):
         os.rename(
             filename,
-            os.path.join(path, f"({1471 + idx})" + suffix)
+            os.path.join(path, f"({1471 + idx})" + suffix) #add the starting number
         )
 
 if __name__=="__main__":
     main()
-#0
